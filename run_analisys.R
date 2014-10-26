6bf6593f6aa9d763ca1b56610365a94b7d64f90e
Dani-P-S <daniel.cimne@gmail.com>
2014-10-26 22:51:27 +0100

    final version 2

--- run_analisys.R
+++ run_analisys.R
@@ -5,33 +5,79 @@
 # - Appropriately labels the data set with descriptive variable names. 
 # - From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
+#setwd('Documents/github/Getting-and-Cleaning-Data-Course-Project')
 setwd('UCI HAR Dataset/')
 
-# subject files
-file_train_1 <- 'train/subject_train.txt'
-file_test_1  <- 'test/subject_test.txt'
-data_train_1 <- data.frame(read.csv(file_train_1, header = F, sep = ""))
-data_test_1 <- data.frame(read.csv(file_test_1, header = F,sep = ""))
-if (!file.exists('merged_data/')) dir.create('merged_data/',showWarnings = FALSE)
-data_subject <- rbind(data_train_1[], data_test_1[])
+### Merging the training and testing data into a one data set called 'merged'
+list_file_names <- c('subject','X','y')
+for (file in list_file_names){
+  file_train <- paste('train/', file, '_train.txt', sep = "")
+  file_test  <- paste('test/', file, '_test.txt', sep = "")
+  data_train <- read.csv(file_train, header = F, sep = "")
+  data_test <- read.csv(file_test, header = F, sep = "")
+  merged_data <- rbind(data_train,data_test)
+  dirname <- 'merged_data/'
+  if (!file.exists(dirname)) dir.create(dirname,showWarnings = FALSE)
+  write.table(row.names = F, col.names = F, merged_data, file = paste(dirname, file, '_merged.txt', sep = ""), sep = " ", qmethod = "double")
+}
 
-# X_ files
-file_train_2 <- 'train/X_train.txt'
-file_test_2  <- 'test/X_test.txt'
-data_train_2 <- data.frame(read.csv(file_train_2, header = F, sep = ""))
-data_test_2 <- data.frame(read.csv(file_test_2, header = F, sep = ""))
-names(data_test_subject) = 'subject'
-names(data_train_subject) = 'subject'
-if (!file.exists('merged_data/')) dir.create('merged_data/',showWarnings = FALSE)
-data_2 <- rbind(data_train_2[], data_test_2[])
+list_file_names <- c('body_acc_x','body_acc_y','body_acc_z','body_gyro_x','body_gyro_y','body_gyro_z','total_acc_x','total_acc_y','total_acc_z')
+for (file in list_file_names){
+  file_train <- paste('train/Inertial Signals/', file, '_train.txt', sep = "")
+  file_test  <- paste('test/Inertial Signals/', file, '_test.txt', sep = "")
+  data_train <- read.csv(file_train, header = F, sep = "")
+  data_test <- read.csv(file_test, header = F, sep = "")
+  merged_data <- rbind(data_train,data_test)
+  dirname <- 'merged_data/Inertial Signals/'
+  if (!file.exists(dirname)) dir.create(dirname,showWarnings = FALSE)
+  write.table(row.names = F, col.names = F, merged_data, file = paste(dirname, file, '_merged.txt', sep = ""), sep = " ", qmethod = "double")
+}
 
-# y_ files
-file_train_3 <- 'train/y_train.txt'
-file_test_3  <- 'test/y_test.txt'
-data_train_3 <- data.frame(read.csv(file_train_3, header = F, sep = " "))
-data_test_3 <- data.frame(read.csv(file_test_3, header = F, sep = " "))
-names(data_test_subject) = 'subject'
-names(data_train_subject) = 'subject'
-if (!file.exists('merged_data/')) dir.create('merged_data/',showWarnings = FALSE)
-data_3 <- rbind(data_train_3[], data_test_3[])
+
+### Adding column names (features)
+dirname <- 'merged_data/'
+file <- 'X'
+file <- paste(dirname, file, '_merged.txt', sep = "")
+data <- read.csv(file, header = F, sep = "")
+file_features <- 'features.txt'
+data_names <- read.csv(file_features, header = F, sep = "")
+names(data) <- data_names[,2]
+
+### Creating activities description data frame
+file_activities <- 'activity_labels.txt'
+activities_names <- read.csv(file_activities, header = F, sep = "")
+dirname <- 'merged_data/'
+file <- 'y'
+file <- paste(dirname, file, '_merged.txt', sep = "")
+data_activities <- read.csv(file, header = F, sep = "")
+data_activities <- data.frame(factor(data_activities[,1],levels = activities_names[,1], labels =  activities_names[,2]))
+names(data_activities) <- c('activityName')
+
+### Creating subject data frame
+dirname <- 'merged_data/'
+file <- 'subject'
+file <- paste(dirname, file, '_merged.txt', sep = "")
+data_subject <- read.csv(file, header = F, sep = "")
+names(data_subject) <- c('subject')
+
+#### Extracting the measurements on the mean 'mean()' and standard deviation 'std()' for each measurement. 
+index <- 1
+index_list <- numeric(0)
+for (i in data_names[,2]) {
+  name <- unlist(strsplit(as.character(i),'-'))
+  if (length(name) >= 2) {
+    if (name[2] == 'mean()' || name[2] == 'std()') {
+      index_list <- append(index_list,index)
+    }  
+  }
+  index <- index + 1
+}
+data_new <- data[,index_list]
+
+
+### Creating the new data set and saving the data set into a file
+data_new <- cbind(data_subject,data_activities,data_new)
+filename <- 'new_data_set.txt'
+write.table(row.names = F, col.names = T, data_new, file = filename, sep = " ", qmethod = "double")
+
 
